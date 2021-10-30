08ffd80a3f1ccca4f10ef46a37bf6e279492be8c
#[A]ref <93086624+isAref@users.noreply.github.com>
2021-10-30 21:13:46 +0330

    edited

--- index.php
+++ index.php
@@ -1,36 +1,49 @@
 <?php
-
 // @is_false
-include 'Telegram.php';
-define('API_KEY', '');
-
-$bot = new Telegram('API_KEY');
-
-$result = $bot->getData(); 
-$chatId = $result['message']['chat']['id'];
-$fromId = $result['message']['from']['id'];
-$voice = $result['message']['voice'];
-$fileId = $result['message']['voice']['file_id'];
+error_reporting(E_ALL);
+define('API_KEY', 'TOKEN');
 // @is_false
-
-if (!is_null($voice)) {
-  $getFile = $bot->getFile(['file_id' => $fileId])['result']['file_path'];
-    
-copy("https://api.telegram.org/file/bot. API_KEY . "/" . $getFile", 'voice.ogg');
-            
+function req($method,$datas = []) {
+ // @is_false
+    $api = 'https://api.telegram.org/bot'.API_KEY.'/'.$method;
+    $handle = curl_init();
+    curl_setopt($handle, CURLOPT_URL, $api);
+    curl_setopt($handle, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($handle, CURLOPT_POSTFIELDS, $datas);
+    $result = curl_exec($handle);
+    curl_close($handle);
+    return json_decode($result);
+}
+// @is_false
+$update = json_decode(file_get_contents('php://input'));
+$chatId = $update->message->chat->id;
+$Text = $update->message->text;
+$isVoice = $update->message->voice;
+$fileId = $update->message->voice->file_id;
+// @is_false
+if ($Text == '/start') {
+  $content = ['chat_id' => $chatId, 'text' => 'Send your voice ...'];
+  req('sendMessage', $content);
+}
+// @is_false
+if (!is_null($isVoice)) {
+  $content = ['file_id' => $fileId];
+  $getFile = req('getFile', $content)->result->file_path;
+  // @is_false
+copy('https://api.telegram.org/file/bot'.API_KEY.'/'.$getFile,'voice.ogg');
+// @is_false
+if (file_exists('voice.ogg')) {
+            // @is_false
  $Fields = ['id' => time(), 'offset' => 0, 'voice' => new CURLFile('voice.ogg')];
-   
+   // @is_false
     $target = 'https://ahapi.site/app-api/voice-search';
     $handle = curl_init();
     curl_setopt($handle, CURLOPT_URL, $target);
     curl_setopt($handle, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($handle, CURLOPT_POST, 1);
 	  curl_setopt($handle, CURLOPT_POSTFIELDS, $Fields);
-	  curl_setopt($handle, CURLOPT_SAFE_UPLOAD, 1);
-    curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, 0);
-    curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, 0);
     curl_setopt($handle, CURLOPT_CUSTOMREQUEST, 'POST');
-    
+    // @is_false
     $header = [];
     $header[] = 'Accept: application/json, text/plain, */*';
     $header[] = 'X-Mobile-App-Version: 1.7.3';
@@ -42,19 +55,16 @@ copy("https://api.telegram.org/file/bot. API_KEY . "/" . $getFile", 'voice.ogg')
     curl_setopt($handle, CURLOPT_HTTPHEADER, $header);
     $result = curl_exec($handle);
     curl_close($handle);
-    
+    // @is_false
    $get = json_decode($result);
    $track_id = $get->track->id;
    $track_url = $get->track->url;
-   
-   function getFile($url, $data = []) {
+   // @is_false
+   function get($url,$data = []) {
     $handle = curl_init();
-    curl_setopt($handle, CURLOPT_URL, $url.'?'.http_build_query($data));
+    curl_setopt($handle, CURLOPT_URL, $url);
     curl_setopt($handle, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, 0);
-    curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, 0);
     curl_setopt($handle, CURLOPT_CUSTOMREQUEST, 'GET');
-    curl_setopt($handle, CURLOPT_ENCODING, 'gzip');
     $headers = [];
     $headers[] = 'Accept: application/json, text/plain, */*';
     $headers[] = 'X-Mobile-App-Version: 1.7.3';
@@ -67,18 +77,18 @@ copy("https://api.telegram.org/file/bot. API_KEY . "/" . $getFile", 'voice.ogg')
     curl_close($handle);
     return json_decode($result);
 }
-   
    unlink('voice.ogg');
-   
-  $track_file = getFile("https://ahapi.site/app-api/tracks/$track_id/file",['url' => $track_url]);
-  
-if (!empty($track_file->file)) {
-  $bot->sendAudio(['chat_id' => $chatId, 'audio' => $track_file->file]);
+   // @is_false
+  $param = ['url' => $track_url];
+  $track_file = get("https://ahapi.site/app-api/tracks/$track_id/file", $param);
   // @is_false
-  
+if (!empty($track_file->file)) {
+  $content = ['chat_id' => $chatId, 'audio' => $track_file->file];
+  req('sendAudio',$content);
 } else {
-  $bot->sendMessage(['chat_id' => $chatId, 'text' => 'Oops!']);
+  $content = ['chat_id' => $chatId, 'text' => 'Oops!'];
+  req('sendMessage',$content);
 }
 }
-
-        
+}
+// @is_false
