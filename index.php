26f2ca224de190b3b2f8a2303fe53c1f5fdbff4d
#[A]ref <93086624+isAref@users.noreply.github.com>
2021-10-31 11:17:03 +0330

    Bug Fixed
    Some bugs were fixed and the source was optimized.

--- index.php
+++ index.php
@@ -1,41 +1,46 @@
 <?php
-// @is_false
 error_reporting(E_ALL);
+
+/*
+* Author: Aref
+* Telegram: @is_false
+* Version: 1
+*/
+
 define('API_KEY', 'TOKEN');
-// @is_false
-function req($method,$datas = []) {
- // @is_false
+
+function req($method,$data = []) {
     $api = 'https://api.telegram.org/bot'.API_KEY.'/'.$method;
     $handle = curl_init();
     curl_setopt($handle, CURLOPT_URL, $api);
     curl_setopt($handle, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($handle, CURLOPT_POSTFIELDS, $datas);
-    $result = curl_exec($handle);
+    curl_setopt($handle, CURLOPT_POSTFIELDS, $data);
+    $result = json_decode(curl_exec($handle));
     curl_close($handle);
-    return json_decode($result);
+    return $result;
 }
-// @is_false
+
 $update = json_decode(file_get_contents('php://input'));
 $chatId = $update->message->chat->id;
 $Text = $update->message->text;
 $isVoice = $update->message->voice;
 $fileId = $update->message->voice->file_id;
-// @is_false
+
 if ($Text == '/start') {
-  $content = ['chat_id' => $chatId, 'text' => 'Send your voice ...'];
+  $content = ['chat_id' => $chatId, 'text' => '• Send your voice ...'];
   req('sendMessage', $content);
 }
-// @is_false
+
 if (!is_null($isVoice)) {
   $content = ['file_id' => $fileId];
   $getFile = req('getFile', $content)->result->file_path;
-  // @is_false
+  
 copy('https://api.telegram.org/file/bot'.API_KEY.'/'.$getFile,'voice.ogg');
-// @is_false
+
 if (file_exists('voice.ogg')) {
-            // @is_false
+            
  $Fields = ['id' => time(), 'offset' => 0, 'voice' => new CURLFile('voice.ogg')];
-   // @is_false
+   
     $target = 'https://ahapi.site/app-api/voice-search';
     $handle = curl_init();
     curl_setopt($handle, CURLOPT_URL, $target);
@@ -43,45 +48,45 @@ if (file_exists('voice.ogg')) {
     curl_setopt($handle, CURLOPT_POST, 1);
 	  curl_setopt($handle, CURLOPT_POSTFIELDS, $Fields);
     curl_setopt($handle, CURLOPT_CUSTOMREQUEST, 'POST');
-    // @is_false
-    $header = [];
-    $header[] = 'Accept: application/json, text/plain, */*';
-    $header[] = 'X-Mobile-App-Version: 1.7.3';
-    $header[] = 'X-Mobile-App-Market: google-play';
-    $header[] = 'user-agent: Ahangify Mobile/1.7.3 (Samsung SM-A217F, Android 11 "30")';
-    $header[] = 'X-Language: en';
-    $header[] = 'authorization:Bearer '.file_get_contents('auth.txt');
-    // @is_false
-    curl_setopt($handle, CURLOPT_HTTPHEADER, $header);
-    $result = curl_exec($handle);
+    $headers = [
+     'Accept: application/json, text/plain, */*',
+     'X-Mobile-App-Version: 1.7.3','X-Mobile-App-Market: google-play',
+     'user-agent: Ahangify Mobile/1.7.3 (Samsung SM-A217F, Android 11 "30")',
+     'X-Language: en',
+     'authorization:Bearer '.file_get_contents('auth.txt')
+     ];
+    
+    curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
+    $result = json_decode(curl_exec($handle));
     curl_close($handle);
-    // @is_false
-   $get = json_decode($result);
-   $track_id = $get->track->id;
-   $track_url = $get->track->url;
-   // @is_false
+    
+   $track_id = $result->track->id;
+   $track_url = $result->track->url;
+   
    function get($url,$data = []) {
     $handle = curl_init();
     curl_setopt($handle, CURLOPT_URL, $url);
     curl_setopt($handle, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($handle, CURLOPT_CUSTOMREQUEST, 'GET');
-    $headers = [];
-    $headers[] = 'Accept: application/json, text/plain, */*';
-    $headers[] = 'X-Mobile-App-Version: 1.7.3';
-    $headers[] = 'X-Mobile-App-Market: google-play';
-    $headers[] = 'user-agent: Ahangify Mobile/1.7.3 (Samsung SM-A217F, Android 11 "30")';
-    $headers[] = 'X-Language: en';
-    $headers[] = 'authorization:Bearer '.file_get_contents('auth.txt');
+    
+    $headers = [
+     'Accept: application/json, text/plain, */*',
+     'X-Mobile-App-Version: 1.7.3','X-Mobile-App-Market: google-play',
+     'user-agent: Ahangify Mobile/1.7.3 (Samsung SM-A217F, Android 11 "30")',
+     'X-Language: en',
+     'authorization:Bearer '.file_get_contents('auth.txt')
+     ];
+     
     curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
-    $result = curl_exec($handle);
+    $result = json_decode(curl_exec($handle));
     curl_close($handle);
-    return json_decode($result);
+    return $result;
 }
    unlink('voice.ogg');
-   // @is_false
+   
   $param = ['url' => $track_url];
   $track_file = get("https://ahapi.site/app-api/tracks/$track_id/file", $param);
-  // @is_false
+  
 if (!empty($track_file->file)) {
   $content = ['chat_id' => $chatId, 'audio' => $track_file->file];
   req('sendAudio',$content);
@@ -91,4 +96,9 @@ if (!empty($track_file->file)) {
 }
 }
 }
-// @is_false
+
+/*
+* Author: Aref
+* Telegram: @is_false
+* Version: 1
+*/
